c96e21ba3984bd3d20711cb096802f9705983f19
cmays20 <cmays@outlook.com>
2019-04-24 00:27:02 -0500

    Initial Load

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,17 +3,17 @@
 set -e
 
 if [ "$ENABLE_JMX" = "true" ]; then
-    JMX_OPTS = "-Dcom.sun.management.jmxremote=true -Dcom.sun.management.jmxremote.port=9090 -Dcom.sun.management.jmxremote.rmi.port=9090 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false -Djava.rmi.server.hostname=$(hostname -i)"
+    JMX_OPTS="-Dcom.sun.management.jmxremote=true -Dcom.sun.management.jmxremote.port=9090 -Dcom.sun.management.jmxremote.rmi.port=9090 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false -Djava.rmi.server.hostname=$(hostname -i)"
 fi
 
 if [ -n "$USER" ]; then
-    USER_OPTS1 = "su -c"
-    USER_OPTS2 = "${USER} -- "
+    USER_OPTS1="su -c"
+    USER_OPTS2="${USER} -- "
 fi
 
 if [ -z "$1" -o  "${1:0:1}" = '-' ]; then
-    CMD = "java ${JAVA_OPTS} ${JMX_OPTS} -Djava.security.egd=file:/dev/./urandom -jar ${APP_HOME}/${APP_JAR}"
-    exec ${USER_OPTS1} ${CMD} ${USER_OPTS2} "$@"
+    CMD="java ${JAVA_OPTS} ${JMX_OPTS} -Djava.security.egd=file:/dev/./urandom -jar ${APP_HOME}/${APP_JAR}"
+    exec $USER_OPTS1 $CMD $USER_OPTS2 "$@"
 fi
 
 exec "$@"
